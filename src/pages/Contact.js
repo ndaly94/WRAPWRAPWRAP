7860a95b18222b4cff6786a0a2df4607f84cd802
ndaly94 <ndaly94@gmail.com>
2023-09-11 12:21:05 -0400

    framework created

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,7 @@ export default function Contact(props) {
 			<center>
 			<h1>This is the {props.page} page</h1>
 			<div>
-				<img src="https://media.git.generalassemb.ly/user/15881/files/8b5cd280-1f37-11ea-9c27-c9bb294aa61f"/>
-				</div>
-				<div>
-				<img src="https://lh4.googleusercontent.com/Wp6jVNMlvYa2Q8BZ6kIzCbbasfZmNSaWIBJjLKEIDzYB9O6skIt3zyrpqPUjTSXG9WQF23WihWl43h_9UHZtugB96EAulCDDqNtB-KtWYWXJbTTMgHGrG7WU881GACAsrZXe8pJlux6xqGlB46AQliYn=s2048"/>
+				<p>For any questions please contact Nick @ NDaly94@gmail.com</p>
 			</div>			
 			</center>
 		</div>
